b27627e881a5de8af9a3770bf72eec742fb4b027
finanovita <finanovita479@gmail.com>
2024-05-14 14:44:32 +0700

    Memanggil objek dari destruktor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -44,9 +44,12 @@ void angka::isiData() {
 }
 
 
-int main()
-{
-    
-}
+int main() {
+    angka belajarcpp(3); //Constructor Dipanggil 
+    angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+    delete ptrBelajarcpp; //Destructor Dipanggil
+
+    return 0;
+}//Destructor Dipanggil
 
 
