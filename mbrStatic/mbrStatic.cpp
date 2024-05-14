28e6c7f3c445a83daf726e5da0d7f5eec8099703
finanovita <finanovita479@gmail.com>
2024-05-14 14:13:31 +0700

    Membuat class mahasiswa pada member static

--- mbrStatic/mbrStatic.cpp
+++ mbrStatic/mbrStatic.cpp
@@ -5,6 +5,21 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa{
+
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
+};
+
 
 int main()
 {
