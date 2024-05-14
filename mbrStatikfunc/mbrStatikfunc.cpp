1964ac329d66c78a92d97de2f0675bf6eccd957d
finanovita <finanovita479@gmail.com>
2024-05-14 14:28:05 +0700

    Membuat class mahasiswa member statik function

---
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -0,0 +1,41 @@
+// mbrStatikfunc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi Function*/ }
+
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 169;
+
+void mahasiswa::setID() {
+    id = ++nim;
+}
+
+void mahasiswa::printAll() {
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+
+int main()
+{
+    
+}
+
