c257f26278628a8b27b8e3088d243bfc82778fc7
finanovita <finanovita479@gmail.com>
2024-05-14 14:48:37 +0700

    chain function calls

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -22,9 +22,13 @@ public:
 } bukunya;
 
 
-int main()
+int main() 
 {
-    
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+
 }
 
 
