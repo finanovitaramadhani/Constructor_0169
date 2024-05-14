33531f04af039ca10e669decbbddf17ea849be28
finanovita <finanovita479@gmail.com>
2024-05-14 14:47:04 +0700

    Membuat class buku pada chainfunction

---
+++ chainfunction/chainfunction.cpp
@@ -0,0 +1,30 @@
+// chainfunction.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+
+} bukunya;
+
+
+int main()
+{
+    
+}
+
+
